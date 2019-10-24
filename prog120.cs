64e8fdf1de6c06ecf0eda607ad74cb6269f66098
lizamashchenko <55406171+lizamashchenko@users.noreply.github.com>
2019-10-24 19:37:49 +0300

    Add files via upload

---
+++ prog120.cs
@@ -0,0 +1,56 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acmp.ru
+{
+    class prog120
+    {
+        static void Main()
+        {
+            Pr120();
+        }
+
+        static void Pr120()
+        {
+
+            Random r = new Random();
+            int m = 3;
+            int n = 4;
+            int [,]a = new int[m, n];
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    a[i, j] = r.Next(200);
+                }
+            }
+            int[,] b = new int[m, n];
+
+
+
+            for (int j = 2; j < n; j++)
+            {
+                b[1, j] = a[1, j] + b[1, j - 1];
+            }
+            for (int i = 2; i < m; i++)
+            {
+                b[i, 1] = a[i, 1] + b[i - 1, 1];
+            }
+
+
+
+            for (int i = 2; i < m; i++)
+            {
+                for (int j = 2; j < n; j++)
+                {
+                    if (b[i, j - 1] <= b[i - 1, j]) b[i, j] = a[i, j] + b[i, j - 1];
+                    else b[i, j] = a[i, j] + b[i - 1, j];
+                }
+            }
+            Console.WriteLine(b[m, n]);
+        }
+    }
+}*/
---
+++ prog329.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _329
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pr329();
+        }
+        static void Pr329()
+        {
+            int N = 5;
+            Random r = new Random();
+            int[] a = new int[N];
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(1000) - r.Next(1000);
+                Console.Write(a[i] + ' ');
+            }
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < 0)
+                {
+                    Console.Write(i + ' ');
+                    sum = sum + a[i];
+                }
+                else if (a[i] < 0 && a[i + 1] < 0)
+                {
+                    if (a[i] < a[i + 1])
+                    {
+                        Console.Write(i + ' ');
+                        sum = sum + a[i];
+                    }
+                    else
+                    {
+                        i++;
+                        Console.WriteLine(i + ' ');
+                        sum = sum + a[i];
+                    }
+                }
+                else
+                {
+                    i++;
+                    Console.Write(i);
+                    sum = sum + a[i];
+                }
+            }
+            Console.WriteLine("Sum" + sum);
+        }
+    }
+}
+
